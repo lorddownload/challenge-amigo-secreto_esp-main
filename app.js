6ea538597438770506cf11fd939e46ee2d2c38bc
Fernando <fflores@dgs.cl>
2025-09-21 02:15:23 -0300

    Update app.js
    desafio

--- app.js
+++ app.js
@@ -1,1 +1,39 @@
-// El principal objetivo de este desafío es fortalecer tus habilidades en lógica de programación. Aquí deberás desarrollar la lógica para resolver el problema.
+let amigos = [];
+
+function agregarAmigo() {
+    let amigoInput = document.getElementById('amigo');
+    let nombreAmigo = amigoInput.value;
+
+    if (nombreAmigo === "") {
+        alert("Por favor, inserte un nombre.");
+        return;
+    }
+
+    amigos.push(nombreAmigo);
+    actualizarLista();
+    amigoInput.value = "";
+}
+
+function actualizarLista() {
+    let lista = document.getElementById('listaAmigos');
+    lista.innerHTML = "";
+
+    for (let i = 0; i < amigos.length; i++) {
+        let item = document.createElement('li');
+        item.textContent = amigos[i];
+        lista.appendChild(item);
+    }
+}
+
+function sortearAmigo() {
+    if (amigos.length < 2) {
+        alert("Agrega al menos dos amigos para poder sortear.");
+        return;
+    }
+
+    let indiceAleatorio = Math.floor(Math.random() * amigos.length);
+    let amigoSorteado = amigos[indiceAleatorio];
+
+    let resultado = document.getElementById('resultado');
+    resultado.innerHTML = `El amigo secreto es: <strong>${amigoSorteado}</strong>`;
+}
